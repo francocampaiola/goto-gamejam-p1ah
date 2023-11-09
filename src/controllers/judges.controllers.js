c12c9941dda1bf37f86a534fb983b11a52c52f4b
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-09 19:41:35 -0300

    Se retira getJudges en JudgesControllers

--- src/controllers/judges.controllers.js
+++ src/controllers/judges.controllers.js
@@ -2,23 +2,6 @@ import JudgesServices from "../services/judges.services.js";
 import GamesControllers from "./games.controllers.js";
 
 /**
- * Función que retorna todos los jueces
- * @param {*} req
- * @param {*} res
- */
-function getJudges(req, res) {
-  JudgesServices.getJudges()
-    .then((judges) => {
-      return res.status(200).json(judges);
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        msg: err.msg,
-      });
-    });
-}
-
-/**
  * Función que retorna un juez en función de su ID
  * @param {*} req
  * @param {*} res
@@ -94,7 +77,6 @@ async function judgeVote(id) {
 }
 
 export default {
-  getJudges,
   getJudgeById,
   generateVote,
   gamesVoted,
