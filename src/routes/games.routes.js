229f060864d2b9e51f64811955b5813838b2e0a7
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-10 14:41:28 -0300

    Fix en ruta de getGamesByEdition

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -6,7 +6,7 @@ const route = express.Router();
 
 route.get("/games/:id", GamesControllers.getGameById);
 route.get("/games/:id/average", GamesControllers.getGameAverage);
-route.get("/games/:id/edition", GamesControllers.getGamesByEdition);
+route.get("/games/:edition", GamesControllers.getGamesByEdition);
 route.post("/games", [validateCreateGame], GamesControllers.createGame);
 route.put("/games/:id", GamesControllers.editGame);
 route.delete("/games/:id", GamesControllers.deleteGame);
