140df9e0ebc598e920051384efc21e8f63ee96e2
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-11 09:33:02 -0300

    Fix en games.controllers.js. Reinit

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -18,94 +18,6 @@ function getGameById(req, res) {
 }
 
 /**
- * Función que retorna una lista de juegos a partir de la edición.
- * @param {*} req
- * @param {*} res
- */
-function getGamesByEdition(req, res) {
-  GamesServices.getGamesByEdition(req.params.edition, {
-    genre: req.query.genre,
-  })
-    .then((list) => {
-      res.status(200).json(list);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        msg: err.msg,
-      });
-    });
-}
-
-/**
- * Función que retorna una lista de votos recibidos por un juego, en función del ID del juego.
- * @param {*} req
- * @param {*} res
- */
-function getVotesByGame(req, res) {
-  GamesServices.getVotesByGame(req.params.id)
-    .then((vote) => {
-      const result = vote.map((r) => {
-        return {
-          name_judge: r.name_judge,
-          gameplay: r.gameplay,
-          art: r.art,
-          sound: r.sound,
-          affinity: r.affinity,
-        };
-      });
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        msg: err.msg,
-      });
-    });
-}
-
-/**
- * Función que retorna el promedio de votos en un juego en específico a partir de su ID.
- * @param {*} req
- * @param {*} res
- */
-function getGameAverage(req, res) {
-  GamesServices.getVotesByGame(req.params.id)
-    .then((result) => {
-      const average = {
-        name_game: "",
-        gameplay: 0,
-        art: 0,
-        sound: 0,
-        affinity: 0,
-      };
-
-      const lengthList = result.length;
-
-      result.foreach((g) => {
-        average.name_game = g.name_game;
-        average.gameplay += g.gameplay;
-        average.art += g.art;
-        average.sound += g.sound;
-        average.affinity += g.affinity;
-      });
-
-      const totalAverage = {
-        name_game: average.name_game,
-        gameplay: average.gameplay / lengthList,
-        art: average.art / lengthList,
-        sound: average.sound / lengthList,
-        affinity: average.affinity / lengthList,
-      };
-
-      res.status(200).json(totalAverage);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        msg: err.msg,
-      });
-    });
-}
-
-/**
  * Función que agrega un juego. Si lo consigue, lo retorna, de lo contrario, retorna un mensaje de error.
  * @param {*} req
  * @param {*} res
@@ -163,35 +75,16 @@ async function deleteGame(req, res) {
     });
 }
 
-/**
- * Función que actualiza un juego incorporándole el score a partir de sus votaciones.
- * @param {string} id_game
- * @param {number} score
- */
-async function updateGame(id_game, score) {
-  let game = await GamesServices.getGameById(id_game);
-  game.score += score;
-  GamesServices.editGame(id_game, game);
-}
-
 export default {
   getGameById,
-  getGamesByEdition,
-  getVotesByGame,
-  getGameAverage,
   createGame,
   editGame,
-  updateGame,
   deleteGame,
 };
 
 export {
   getGameById,
-  getGamesByEdition,
-  getVotesByGame,
-  getGameAverage,
   createGame,
   editGame,
-  updateGame,
   deleteGame,
 };
