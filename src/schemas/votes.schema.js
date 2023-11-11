4eeb9bf8bbce19ed5e3add81352f5eb6df48ac02
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-11 12:50:20 -0300

    Fix en votes.schema

--- src/schemas/votes.schema.js
+++ src/schemas/votes.schema.js
@@ -7,14 +7,34 @@ const minNumberError = "El valor debe ser mayor a 0";
 const maxNumberError = "El valor debe ser menor o igual a 10";
 
 const createVoteSchema = yup.object({
-    id_judge: yup.string(stringError).required(requiredError),
-    name_judge: yup.string(stringError).required(requiredError),
-    id_game: yup.string(stringError).required(requiredError),
-    name_game: yup.string(stringError).required(requiredError),
-    gameplay: yup.number().integer(integerError).min(1, minNumberError).max(10, maxNumberError).required(requiredError),
-    art: yup.number().integer(integerError).min(1, minNumberError).max(10, maxNumberError).required(requiredError),
-    sound: yup.number().integer(integerError).min(1, minNumberError).max(10, maxNumberError).required(requiredError),
-    affinity: yup.number().integer(integerError).min(1, minNumberError).max(10, maxNumberError).required(requiredError),
-})
+  id_judge: yup.string(stringError).required(requiredError),
+  name_judge: yup.string(stringError).required(requiredError),
+  id_game: yup.string(stringError).required(requiredError),
+  name_game: yup.string(stringError).required(requiredError),
+  gameplay: yup
+    .number()
+    .integer(integerError)
+    .min(1, minNumberError)
+    .max(10, maxNumberError)
+    .required(requiredError),
+  art: yup
+    .number()
+    .integer(integerError)
+    .min(1, minNumberError)
+    .max(10, maxNumberError)
+    .required(requiredError),
+  sound: yup
+    .number()
+    .integer(integerError)
+    .min(1, minNumberError)
+    .max(10, maxNumberError)
+    .required(requiredError),
+  affinity: yup
+    .number()
+    .integer(integerError)
+    .min(1, minNumberError)
+    .max(10, maxNumberError)
+    .required(requiredError),
+});
 
-export default { createVoteSchema };
+export { createVoteSchema };
