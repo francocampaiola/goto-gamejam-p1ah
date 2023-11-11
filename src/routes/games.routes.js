c321320176d1029c50b1821a42ca32b7fc42dc7d
Franco Campaiola <francocampaiola@hotmail.com>
2023-11-11 12:15:24 -0300

    Desarrollo de routes de votes y fix de routes de games

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,12 +1,17 @@
 import express from "express";
 import GamesControllers from "../controllers/games.controllers.js";
+import VotesRoutes from '../routes/votes.routes.js';
 import { validateCreateGame } from '../middlewares/games.middlewares.js'
 
 const route = express.Router();
 
 route.get("/games/:id", GamesControllers.getGameById);
+route.get("/games/bygenre/:genre", GamesControllers.getGamesByGenre);
+route.get("/games/byedition/:edition", GamesControllers.getGamesByEdition);
 route.post("/games", [validateCreateGame], GamesControllers.createGame);
 route.put("/games/:id", GamesControllers.editGame);
 route.delete("/games/:id", GamesControllers.deleteGame);
+
+route.use("/games", VotesRoutes);
 
 export default route;
---
+++ src/routes/votes.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import VotesControllers from "../controllers/votes.controllers.js";
+import {
+  validateCreateVote,
+  uniqueVote,
+  judgeExist,
+  gameExist,
+} from "../middlewares/votes.middlewares.js";
+
+const route = express.Router();
+
+route.get("/:id/votes", VotesControllers.getVotesByGame);
+route.get("/:id/average", VotesControllers.getAverageByGame);
+route.post(
+  "/:id/votes",
+  [validateCreateVote],
+  [gameExist],
+  [judgeExist],
+  [uniqueVote],
+  VotesControllers.createVote
+);
+
+export default route;
